a2c8f878a098bef6905aa82e00b5a27b5ae46fef
YeonJun Cho <micayel1219@gmail.com>
2021-07-21 17:49:14 +0900

    google login add

---
+++ FirebaseAuthAccess/app/src/main/java/com/cyj/firebaseauthaccess/DoneActivity.kt
@@ -0,0 +1,11 @@
+package com.cyj.firebaseauthaccess
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_done)
+    }
+}
