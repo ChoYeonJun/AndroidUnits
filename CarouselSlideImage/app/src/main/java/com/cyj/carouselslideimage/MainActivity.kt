dd4d4fd04708d6b81a20944a1ef50dc30d6f6e9c
YeonJun Cho <micayel1219@gmail.com>
2021-08-29 16:54:17 +0900

    carousel slide image sample project add

---
+++ CarouselSlideImage/app/src/main/java/com/cyj/carouselslideimage/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.cyj.carouselslideimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+
+// Register lifecycle. For activity this will be lifecycle/getLifecycle() and for fragment it will be viewLifecycleOwner/getViewLifecycleOwner().
+        carousel.registerLifecycle(lifecycle)
+
+        val list = mutableListOf<CarouselItem>()
+
+// Image URL with caption
+        list.add(
+            CarouselItem(
+                imageUrl = "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080",
+                caption = "Photo by Aaron Wu on Unsplash"
+            )
+        )
+
+// Just image URL
+        list.add(
+            CarouselItem(
+                imageUrl = "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"
+            )
+        )
+
+// Image URL with header
+        val headers = mutableMapOf<String, String>()
+        headers["header_key"] = "header_value"
+
+        list.add(
+            CarouselItem(
+                imageUrl = "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080",
+                headers = headers
+            )
+        )
+
+// ...
+
+        carousel.setData(list)
+    }
+}
