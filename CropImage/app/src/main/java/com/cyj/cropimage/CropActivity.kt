643b993af8d7b13e7fa4b4e08f49f933ddbde101
YeonJun Cho <micayel1219@gmail.com>
2021-09-04 15:42:18 +0900

    Crop image 파일 앱에서 이미지 선택

--- CropImage/app/src/main/java/com/cyj/cropimage/CropActivity.kt
+++ CropImage/app/src/main/java/com/cyj/cropimage/CropActivity.kt
@@ -1,22 +1,58 @@
 package com.cyj.cropimage
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
+import java.io.ByteArrayOutputStream
+import java.lang.Exception
 
 
 class CropActivity :AppCompatActivity(){
+    lateinit var cropImageView: CropImageView
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cropimage)
 
-        CropImage.activity()
-            .setGuidelines(CropImageView.Guidelines.ON)
-            .setAspectRatio(4, 3)
-            .start(this);
+        cropImageView = findViewById(R.id.cropImageView)
+        button = findViewById(R.id.button2)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.cap)
+        cropImageView.setAspectRatio(4,3)
+        cropImageView.setImageBitmap(bitmap)
+
+        button.setOnClickListener {
+            val cropped = cropImageView.getCroppedImage()
+            var fileName:String?= "myImage"
+            val intent = Intent(this, MainActivity::class.java)
+            try {
+                val stream = ByteArrayOutputStream()
+                cropped.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                val byteArray = stream.toByteArray()
+                intent.putExtra("bitmap", byteArray)
+                Toast.makeText(this, cropped.height.toString(), Toast.LENGTH_SHORT).show()
+
+                stream.close()
+            }catch(e: Exception){
+
+            }
+
+//            startActivityForResult(intent, 102)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+//        CropImage.activity()
+//            .setGuidelines(CropImageView.Guidelines.ON)
+//            .setAspectRatio(4, 3)
+//            .start(this);
 
     }
 
--- CropImage/app/src/main/java/com/cyj/cropimage/MainActivity.kt
+++ CropImage/app/src/main/java/com/cyj/cropimage/MainActivity.kt
@@ -1,27 +1,89 @@
 package com.cyj.cropimage
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
+import com.theartofdev.edmodo.cropper.CropImageView
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var image: ImageView
     lateinit var button: Button
+    lateinit var button2: Button
+    lateinit var cropImageView: CropImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val intent = it.data
+                val imageUris = ArrayList<Uri>()
+                if (it.resultCode == RESULT_OK && intent != null) {
+                    val mClipData = intent.clipData
+                    val mData = intent.data
+                    if (mClipData != null) {
+                        val mArrayUri = arrayOfNulls<Uri>(mClipData.itemCount)
+                        for (i in 0..mClipData.itemCount.minus(1)) {
+                            val item = mClipData.getItemAt(i)
+                            val uri = item.uri
+//                            mArrayUri[i] = uri
+                            imageUris.add(uri)
+                            cropImageView.clearImage()
+                            cropImageView.visibility = View.GONE
+                            image.setImageURI(uri)
+                        }
+                        Log.d("count", mArrayUri.size.toString())
+                    } else if (mData!= null) {
+                        Log.v("LOG_TAG", "get data")
+                        val uri: Uri = mData
+                        imageUris.add(uri)
+                        cropImageView.clearImage()
+                        cropImageView.visibility = View.GONE
+                        image.setImageURI(uri)
+                    }
+                }
+            }
         image = findViewById(R.id.image)
         button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button4)
+        cropImageView = findViewById(R.id.cropImageView)
         button.setOnClickListener {
-            val intent = Intent(this, CropActivity::class.java)
-            startActivityForResult(intent, 101)
+            val intent = Intent()
+            intent.setType("image/*")
+//            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            Intent.createChooser(intent, "Select Picture")
+//            val intent = Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//            intent.action = Intent.ACTION_GET_CONTENT
+//            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            getContent.launch(intent)
+//            startActivity(intent)
+//            getContent.launch(intent)
+            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.cap)
+            cropImageView.setAspectRatio(4, 3)
+            cropImageView.setImageBitmap(bitmap)
+        }
+
+        button2.setOnClickListener {
+            val cropped = cropImageView.getCroppedImage()
+            image.setImageBitmap(cropped)
+            cropImageView.clearImage()
+            cropImageView.visibility = View.GONE
+
         }
 
 
@@ -35,6 +97,9 @@ class MainActivity : AppCompatActivity() {
             Log.d("Uri", uri.toString())
             Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show()
             image.setImageURI(uri?.toUri())
+        } else if (requestCode == 102) {
+            val bitmap = data?.getParcelableExtra<Bitmap>("image")
+            image.setImageBitmap(bitmap)
         }
     }
 
