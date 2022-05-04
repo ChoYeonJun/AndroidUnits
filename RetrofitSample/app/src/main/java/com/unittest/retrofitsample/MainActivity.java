79228002101c41e3e5e0d6085a7ff07ecad36693
YeonJun Cho <micayel1219@gmail.com>
2022-05-04 21:58:41 +0900

    youtube ui button add

--- RetrofitSample/app/src/main/java/com/unittest/retrofitsample/MainActivity.java
+++ RetrofitSample/app/src/main/java/com/unittest/retrofitsample/MainActivity.java
@@ -1,17 +1,22 @@
 package com.unittest.retrofitsample;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.unittest.retrofitsample.melisma.callback.SearchResponse;
 import com.unittest.retrofitsample.melisma.model.LoginReqUserDto;
-import com.unittest.retrofitsample.melisma.model.dto.MusicDto;
+import com.unittest.retrofitsample.melisma.model.vo.MusicVo;
 import com.unittest.retrofitsample.melisma.service.MusicService;
 
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,29 +38,43 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UUID id = UUID.randomUUID();
-                MusicDto model = new MusicDto(id, "url", 10, id);
-                musicService.createMusic(model);
-//                musicService.searchMusics();
-                model.setViews(100);
-                model.setMusicUrl("musicUrl");
-//                musicService.change(model);
-//                retrofit.hello();
-                musicService.update(model);
-                musicService.searchMusics(new CallbackResponse() {
-                    @Override
-                    public void onSearchSuccess(@NonNull List<MusicVo> vos) {
-                        for (MusicVo vo: vos) {
-                            Log.d("onSuccess search", vo.getId().toString());
-                        }
-                    }
+//                UUID id = UUID.randomUUID();
+//                MusicDto model = new MusicDto(id, "url", 10, id);
+//                musicService.createMusic(model);
+//                musicService.searchMusics(new SearchResponse() {
+//                    @Override
+//                    public void onSuccess(@NonNull List<MusicVo> vos) {
+//                        for (MusicVo vo: vos) {
+//                            Log.d("onSuccess search", vo.getId().toString());
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull Throwable throwable) {
+//
+//                    }
+//                });
 
-                    @Override
-                    public void onError(@NonNull Throwable throwable) {
+//                String id = "74e052d2-561a-402d-a8b3-0b6261cb63f9";
+//
+//                musicService.searchMusic(UUID.fromString(id));
+//                model.setViews(100);
+//                model.setMusicUrl("musicUrl");
+////                musicService.change(model);
+////                retrofit.hello();
+//                musicService.update(model);
+            }
+        });
 
-                    }
-                });
+        Button youtube_btn = findViewById(R.id.youtube_btn);
+
+        youtube_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, YoutubeActivity.class));
             }
         });
     }
+
+
 }
--- RetrofitSample/app/src/main/java/com/unittest/retrofitsample/YoutubeActivity.java
+++ RetrofitSample/app/src/main/java/com/unittest/retrofitsample/YoutubeActivity.java
@@ -2,6 +2,7 @@ package com.unittest.retrofitsample;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -13,18 +14,36 @@ public class YoutubeActivity extends YouTubeBaseActivity {
     YouTubePlayerView youTubePlayerView;
     YouTubePlayer.OnInitializedListener listener;
     YouTubePlayer player;
-
+    Button play_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
 
-        youTubePlayerView = findViewById(R.id.youtubeView);
 
+        youTubePlayerView = findViewById(R.id.youtubeView);
+        Button load_btn = findViewById(R.id.load_btn);
+        play_btn = findViewById(R.id.play_btn);
+
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playClick();
+            }
+        });
+
+        load_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initClick();
+            }
+        });
         listener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 player = youTubePlayer;
+                loadClick("str");
+                player.play();
             }
 
             @Override
@@ -32,17 +51,20 @@ public class YoutubeActivity extends YouTubeBaseActivity {
 
             }
         };
+
+        youTubePlayerView.initialize("none",listener);
+
     }
 
-    public void initClick(View v){
+    public void initClick(){
         youTubePlayerView.initialize("none",listener);
     }
 
-    public void loadClick(View v, String code){
-        player.loadVideo(code);
+    public void loadClick(String code){
+        player.loadVideo("1cB5BREyq9Y");
     }
 
-    public void playClick(View v){
+    public void playClick(){
         if(player.isPlaying()) player.pause();
         else player.play();
     }
